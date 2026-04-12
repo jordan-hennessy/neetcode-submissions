79293049eb8cf3ee2eb4fe49f0391ac0e497dda9
Jordan <151176205+jordan-hennessy@users.noreply.github.com>
2026-04-12 13:26:23 +0100

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        encoded = []
+
+        for s in strs:
+            prefix = f"{len(s):04}"
+            encoded.append(prefix + s)
+        
+        return "".join(encoded)
+
+    def decode(self, s: str) -> List[str]:
+
+        decoded = []
+        index = 0
+
+        while index < len(s):
+            s_len = int(s[index : index + 4])
+            index += 4
+
+            word = s[index : index + s_len]
+            decoded.append(word)
+            index += s_len
+
+        return decoded
