5e6baca6f387b8ddd66270a385fb51dedac97c91
Jordan <151176205+jordan-hennessy@users.noreply.github.com>
2026-07-08 12:40:01 +0100

    Add: level-order-traversal-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-2.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+from collections import deque
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+
+            # Uses BFS 
+            # We want to create a list of every number on each layer.
+            # Can't fully remember how to do BFS, but I know what it is
+            # Add numbers on a layer to a list and append that list to a result list before moving onto the next layer
+            # The values don't matter to us here.
+            # A node might be None but there could be more nodes in the tree on that level.
+        
+        res = []
+
+        if not root:
+            return []
+        
+        queue = deque([root])
+        layer = []
+
+        while queue:
+
+            layer_size = len(queue)
+
+            for i in range(layer_size):
+                node = queue.popleft()
+                layer.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            res.append(layer)
+            layer = []
+
+        return res
+
+
+
+
+            
