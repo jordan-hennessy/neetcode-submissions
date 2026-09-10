f21ea5e63447039de06ec94e10cf8d2f40b44ba9
Jordan <151176205+jordan-hennessy@users.noreply.github.com>
2026-09-10 07:23:51 +0100

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+
+        pacific, atlantic = set(), set()
+
+        m, n = len(heights), len(heights[0])
+
+        def dfs(i: int, j: int, prev: int, ocean: set):
+            if (i < 0 or j < 0 or
+                i >= m or j >= n or
+                prev > heights[i][j] or
+                (i, j) in ocean
+            ):
+                return
+            ocean.add((i, j))
+
+            dfs(i + 1, j, heights[i][j], ocean)
+            dfs(i - 1, j, heights[i][j], ocean)
+            dfs(i, j + 1, heights[i][j], ocean)
+            dfs(i, j - 1, heights[i][j], ocean)
+
+        lr, lc = m - 1, n - 1
+
+        for i in range(m):
+            dfs(i, 0, heights[i][0], pacific)
+            dfs(i, lc, heights[i][lc], atlantic)
+
+        for j in range(n):
+            dfs(0, j, heights[0][j], pacific)
+            dfs(lr, j, heights[lr][j], atlantic)
+
+        return list(pacific.intersection(atlantic))
